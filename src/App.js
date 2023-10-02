cdd39bf83b241b16b7fcdce1d6077696f4455475
AswinAsok <aswinasokofficial@gmail.com>
2023-10-02 15:19:53 +0530

    feat(website):added-files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TopCoders from "./Pages/TopCoders";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <TopCoders />;
 }
 
 export default App;
