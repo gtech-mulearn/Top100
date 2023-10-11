ee9a52a35ddf8c62b4236d0e075e8f651a6989c9
AswinAsok <aswinasokofficial@gmail.com>
2023-10-11 12:02:53 +0530

    fix(t100c):added-leaderboard-api

--- src/Pages/TopCoders.jsx
+++ src/Pages/TopCoders.jsx
@@ -1,69 +1,91 @@
-import React from 'react'
-import styles from './TopCoders.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "./TopCoders.module.css";
 
 // Images import section
-import heroBar from './assets/heroBar.webp'
-import card from './assets/card.png'
-import cash from './assets/cash.webp'
-import topBorder from './assets/pseudo.webp'
-import videoBg from './assets/vid.webm'
+import heroBar from "./assets/heroBar.webp";
+import card from "./assets/card.png";
+import cash from "./assets/cash.webp";
+import topBorder from "./assets/pseudo.webp";
+import videoBg from "./assets/vid.webm";
 
 // Components import section
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
-import Form from './components/Form/Form'
-import Faq from './components/Faq/Faq'
-import Selection from './components/Selection/Selection'
-import Challenge from './components/Challenge/Challenge'
-import Leaderboard from './components/Leaderboard/Leaderboard'
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Faq from "./components/Faq/Faq";
+import Selection from "./components/Selection/Selection";
+import Challenge from "./components/Challenge/Challenge";
+import Leaderboard from "./components/Leaderboard/Leaderboard";
 
 export default function TopCoders() {
-    return (
-        <div className={styles.topCoders}>
-            <Navbar />
+  const [leaderboard, setLeaderboard] = useState([]);
 
-            <div id='home' className={styles.topCodersHero}>
-                <video className={styles.renderVideo} autoPlay loop muted preload="auto">
-                    <source src={videoBg} type="video/webm"></source>
-                </video>
-                <span className={styles.rect}></span>
-                <span className={styles.rect}></span>
-                <span className={styles.rect}></span>
-                <div className={styles.heroBar}>
-                    <img src={heroBar} alt="" />
-                    <a href='#join'>Join the Top 100 Elite Squad</a>
-                </div>
-            </div>
+  useEffect(() => {
+    fetch("https://mulearn.org/api/v1/top100/leaderboard/")
+      .then((response) => response.json())
+      .then((data) => setLeaderboard(data.response))
+      .catch((error) => console.log(error));
 
-            <div id='about' className={styles.topCodersContent}>
-                <img className={styles.topBorder} src={topBorder} alt="" />
-                <div className={styles.text}>
-                    <div className={styles.textHead}>
-                        <span className={styles.rect2}></span>
-                        <h1>Be the top <b>100</b> of the year 2023</h1>
-                        <span className={styles.rect2}></span>
-                    </div>
-                    <p className={styles.textContent}>Welcome to the Top 100 Coders initiative Recognised by Kerala Govt.
-                        We're on a mission to recognize and empower the best coders in India.
-                        If you're passionate about coding and want to make a significant
-                        impact in the tech community, you're in the right place.</p>
-                    <div className={styles.prize}>
-                        <span>
-                            <p>PRIZES</p>
-                            <img src={cash} alt="" />
-                        </span>
-                        <p>WORTH ₹10,00,000</p>
-                    </div>
-                </div>
-                <img className={styles.card} src={card} alt="" />
-            </div>
+    console.log(leaderboard);
+  }, []);
 
-            <Challenge />
-            <Selection />
-            <Leaderboard />
-            {/* <Form / */}
-            <Faq />
-            <Footer />
+  console.log(leaderboard);
+
+  return (
+    <div className={styles.topCoders}>
+      <Navbar />
+
+      <div id="home" className={styles.topCodersHero}>
+        <video
+          className={styles.renderVideo}
+          autoPlay
+          loop
+          muted
+          preload="auto"
+        >
+          <source src={videoBg} type="video/webm"></source>
+        </video>
+        <span className={styles.rect}></span>
+        <span className={styles.rect}></span>
+        <span className={styles.rect}></span>
+        <div className={styles.heroBar}>
+          <img src={heroBar} alt="" />
+          <a href="#join">Join the Top 100 Elite Squad</a>
         </div>
-    )
+      </div>
+
+      <div id="about" className={styles.topCodersContent}>
+        <img className={styles.topBorder} src={topBorder} alt="" />
+        <div className={styles.text}>
+          <div className={styles.textHead}>
+            <span className={styles.rect2}></span>
+            <h1>
+              Be the top <b>100</b> of the year 2023
+            </h1>
+            <span className={styles.rect2}></span>
+          </div>
+          <p className={styles.textContent}>
+            Welcome to the Top 100 Coders initiative Recognised by Kerala Govt.
+            We're on a mission to recognize and empower the best coders in
+            India. If you're passionate about coding and want to make a
+            significant impact in the tech community, you're in the right place.
+          </p>
+          <div className={styles.prize}>
+            <span>
+              <p>PRIZES</p>
+              <img src={cash} alt="" />
+            </span>
+            <p>WORTH ₹10,00,000</p>
+          </div>
+        </div>
+        <img className={styles.card} src={card} alt="" />
+      </div>
+
+      <Challenge />
+      <Selection />
+      <Leaderboard leaderboard={leaderboard} />
+      {/* <Form / */}
+      <Faq />
+      <Footer />
+    </div>
+  );
 }
--- src/Pages/components/Leaderboard/Leaderboard.jsx
+++ src/Pages/components/Leaderboard/Leaderboard.jsx
@@ -1,110 +1,65 @@
-import styles from './Leaderboard.module.css'
-import arrow from '../../assets/arrow.png'
-import hacker from '../../assets/hacker.png'
-import line from '../../assets/line.png'
+import styles from "./Leaderboard.module.css";
+import arrow from "../../assets/arrow.png";
+import hacker from "../../assets/hacker.png";
+import line from "../../assets/line.png";
+import axios from "axios";
 
-export default function Leaderboard() {
-    return (
-        <div className={styles.leaderboard}>
-            <div className={styles.title}>
-                <div className={styles.text}>
-                    <h1>The “<b>Top 100</b>” hunters</h1>
-                    <p>The List of Elite, who are competing for the top 100 spot</p>
-                </div>
-                <img className={styles.arrow} src={arrow} alt="arrow" />
-            </div>
-            <div className={styles.container}>
-                <div className={styles.top}>
-                    <p className={styles.title}>
-                        Welcome to the top 100 series... <br />
-                        Run the command for leaderboard <br />
-                        <p style={{ color: "#00A2E8" }}>-leadrbrd/reveal</p>
-                    </p>
-                    <img className={styles.line} src={line} alt="" />
-                    <img className={styles.hacker} src={hacker} alt="" />
-                </div>
-                <div className={styles.rankList}>
-                    <div className={styles.card}>
-                        <img className={styles.profileImg} src="" alt="img" />
-                        <div className={styles.cardContent}>
-                            <div className={styles.head}>
-                                <p>Rank 1</p>
-                                <p>Anandha Krishnan S</p>
-                                <div className={styles.point}>
-                                    <span></span>
-                                    <p>+3</p>
-                                </div>
-                            </div>
-                            <ul className={styles.content}>
-                                <li>College: St Joseeph's Pala</li>
-                                <li>District: Kottayam</li>
-                                <li>State: Kerala</li>
-                                <li style={{ color: "#00A2E8" }}>Karma: 3200</li>
-                            </ul>
-                        </div>
-                    </div>
+import { useState, useEffect } from "react";
 
-                    <div className={styles.card}>
-                        <img className={styles.profileImg} src="" alt="img" />
-                        <div className={styles.cardContent}>
-                            <div className={styles.head}>
-                                <p>Rank 1</p>
-                                <p>Anandha Krishnan S</p>
-                                <div className={styles.point}>
-                                    <span></span>
-                                    <p>+3</p>
-                                </div>
-                            </div>
-                            <ul className={styles.content}>
-                                <li>College: St Joseeph's Pala</li>
-                                <li>District: Kottayam</li>
-                                <li>State: Kerala</li>
-                                <li style={{ color: "#00A2E8" }}>Karma: 3200</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div className={styles.card}>
-                        <img className={styles.profileImg} src="" alt="img" />
-                        <div className={styles.cardContent}>
-                            <div className={styles.head}>
-                                <p>Rank 1</p>
-                                <p>Anandha Krishnan S</p>
-                                <div className={styles.point}>
-                                    <span></span>
-                                    <p>+3</p>
-                                </div>
-                            </div>
-                            <ul className={styles.content}>
-                                <li>College: St Joseeph's Pala</li>
-                                <li>District: Kottayam</li>
-                                <li>State: Kerala</li>
-                                <li style={{ color: "#00A2E8" }}>Karma: 3200</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div className={styles.card}>
-                        <img className={styles.profileImg} src="" alt="img" />
-                        <div className={styles.cardContent}>
-                            <div className={styles.head}>
-                                <p>Rank 1</p>
-                                <p>Anandha Krishnan S</p>
-                                <div className={styles.point}>
-                                    <span></span>
-                                    <p>+3</p>
-                                </div>
-                            </div>
-                            <ul className={styles.content}>
-                                <li>College: St Joseeph's Pala</li>
-                                <li>District: Kottayam</li>
-                                <li>State: Kerala</li>
-                                <li style={{ color: "#00A2E8" }}>Karma: 3200</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+export default function Leaderboard({ leaderboard }) {
+  return (
+    <div className={styles.leaderboard}>
+      <div className={styles.title}>
+        <div className={styles.text}>
+          <h1>
+            The “<b>Top 100</b>” hunters
+          </h1>
+          <p>The List of Elite, who are competing for the top 100 spot</p>
         </div>
-    )
+        <img className={styles.arrow} src={arrow} alt="arrow" />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <p className={styles.title}>
+            Welcome to the top 100 series... <br />
+            Run the command for leaderboard <br />
+            <p style={{ color: "#00A2E8" }}>-leadrbrd/reveal</p>
+          </p>
+          <img className={styles.line} src={line} alt="" />
+          <img className={styles.hacker} src={hacker} alt="" />
+        </div>
+        <div className={styles.rankList}>
+          {leaderboard.slice(0, 4).map((data) => (
+            <div className={styles.card} key={data.rank}>
+              {/* <img
+                className={styles.profileImg}
+                src={`https://example.com/profile/${data.rank}`}
+                alt="img"
+              /> */}
+              <div className={styles.cardContent}>
+                <div className={styles.head}>
+                  <p>Rank {data.rank}</p>
+                  <p>
+                    {data.first_name} {data.last_name}
+                  </p>
+                  {/* <div className={styles.point}>
+                    <span></span>
+                    <p>+3</p>
+                  </div> */}
+                </div>
+                <ul className={styles.content}>
+                  <li>College: {data.org_title}</li>
+                  <li>District: {data.district_name}</li>
+                  <li>State: {data.state_name}</li>
+                  <li style={{ color: "#00A2E8" }}>
+                    Karma: {data.total_karma}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
