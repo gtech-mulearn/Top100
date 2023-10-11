7f2104f806b83ac41104f2a0e5ae782ae5fe9e65
Edwinliby <edwinliby30@gmail.com>
2023-10-11 22:01:30 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Leaderboard from "./Pages/Leadboard/Leadboard";
+import Leaderboard from "./Pages/Leadboard/board";
 import TopCoders from "./Pages/TopCoders";
 
 function App() {
