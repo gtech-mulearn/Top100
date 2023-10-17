3dd2882ee27f00c8be8dba0a7fe0a39c97a61c09
Edwinliby <edwinliby30@gmail.com>
2023-10-18 01:05:59 +0530

    nav links fixed challenge text changed

--- src/Pages/components/Navbar/Navbar.jsx
+++ src/Pages/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import top100 from "../../assets/top100logo.png";
-import gtech from "../../assets/gtech.webp";
-import { useLocation } from "react-router-dom";
+import gtech from "../../assets/navgtech.png";
+import kerala from '../../assets/kerala.png'
+// import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const navbar = React.useRef(null);
-  const location = useLocation();
-  const path = location.pathname;
+  // const location = useLocation();
+  // const path = location.pathname;
 
   React.useEffect(() => {
     var prevScrollpos = window.pageYOffset;
@@ -31,6 +32,9 @@ export default function Navbar() {
           <a href="/leaderboard">Top 100</a>
         </div>
         <div className={styles.navbarlinks}>
+          <a href="https://startupmission.kerala.gov.in/" target="_blank" rel="noreferrer">
+            <img className={styles.kerala} src={kerala} alt="" />
+          </a>
           <a href="https://mulearn.org/" target="_blank" rel="noreferrer">
             <img className={styles.gtechLogo} src={gtech} alt="" />
           </a>
